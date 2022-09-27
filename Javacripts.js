828392bfe7b2d9df661bad2e46b1d6b3751d44df
Pendusky Sampson <102203758+pendusky@users.noreply.github.com>
2022-09-28 00:18:44 +0100

    Add files via upload

---
+++ Javacripts.js
@@ -0,0 +1,24 @@
+
+        alert('Welcome to Sterling Self Service Center')
+        
+        function open1() {
+        var open1 =
+        window.open('http://127.0.0.1:5501/homepage.html?number=&submit=Continue#','','scrollbars=yes,height=600,width=800,resizable=yes');
+        }
+        function open2() {
+        var open2 =
+        window.open('https://sterling.ng/complaints/','','scrollbars=yes,height=600,width=800,resizable=yes');
+        }
+        function open3() {
+        var open3 =
+        window.open('https://sterling.ng/advisory/','','scrollbars=yes,height=600,width=800,resizable=yes');
+        }
+        function open4() {
+        var open4 =
+        window.open('https://sterling.ng/support/feedback/','','scrollbars=yes,height=600,width=800,resizable=yes');
+        }
+        function open5() {
+        var open5 =
+        window.open('https://sterling.ng/branches/','','scrollbars=yes,height=600,width=800,resizable=yes');
+        }
+       
